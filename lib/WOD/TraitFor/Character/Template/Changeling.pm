279dbd65764f7cc9ed21624c6f4eb7cab87bd6f4
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2012-07-04 21:25:19 -0500

    add more changeling specific stuff

--- lib/WOD/TraitFor/Character/Template/Changeling.pm
+++ lib/WOD/TraitFor/Character/Template/Changeling.pm
@@ -11,6 +11,60 @@ has wyrd => (
    default => quote_sub q{ 1 },
 );
 
+has glamour => (
+   is => 'rw',
+   lazy => 1,
+   default => quote_sub q{
+      use POSIX 'ceil';
+      ceil($_[0]->max_glamour / 2)
+   },
+);
+
+our @max_attr_skill_contract_from_wyrd = (
+   undef,
+   5,
+   5,
+   5,
+   5,
+   5,
+   6,
+   7,
+   8,
+   9,
+   10,
+);
+
+our @max_glamour_per_turn_from_wyrd = (
+   undef,
+   1,
+   2,
+   3,
+   4,
+   5,
+   6,
+   7,
+   8,
+   10,
+   15,
+);
+
+our @max_glamour_from_wyrd = (
+   undef,
+   10,
+   11,
+   12,
+   13,
+   14,
+   15,
+   20,
+   30,
+   50,
+   100
+);
+
+sub max_glamour { $max_glamour_from_wyrd[$_[0]->wyrd] }
+sub max_glamour_per_turn { $max_glamour_per_turn_from_wyrd[$_[0]->wyrd] }
+
 around _reconstruct => sub {
    my ($orig, $self ) = @_;
 
